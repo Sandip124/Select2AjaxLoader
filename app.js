dcf942b50613120c942239419631cfcadfa8e002
Sandip124 <37034590+Sandip124@users.noreply.github.com>
2021-02-04 01:53:25 +0545

    add method for  single element loader

--- app.js
+++ app.js
@@ -51,3 +51,55 @@ function Select2AjaxLoader(parent,child,url,failErrorMessage,valueMember,display
     });
     $(parent).change();
 }
+
+
+function Select2AjaxLoader(element,url,failErrorMessage,valueMember,displayMember,showAllOption = false)
+{
+    let className = [...Array(30)].map(() => Math.random().toString(36)[2]).join('');
+    $(element).before('<div class='+className+'></div>');
+    let loadingIcon = $("."+className);
+    $(loadingIcon).css({
+        'display':'none',
+        'width': '15px',
+        'position': 'relative',
+        'height': '15px',
+        'margin-top': '-15px',
+        'left': 'calc(100% - 3em)',
+        'top': '1.8em',
+        'z-index': '999999999',
+        'border': '2px solid rgb(0 0 0 / 40%)',
+        'border-radius': '50%',
+        'border-top-color': '#fff',
+        'animation': 'spin .5s linear infinite',
+        '-webkit-animation': 'spin .5s linear infinite'
+    });
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            type: 'GET',
+            beforeSend: function ()
+            {
+                $(loadingIcon).css({'display':'block'});
+            },
+            success: function (response) {
+                $(element).empty();
+                if(showAllOption === true)
+                {
+                    $(element).append('<option value="0">ALL</option>');
+                }
+                $.each(response.data, function (i, item) {
+                    let valueHolder = valueMember || "Id";
+                    let displayHolder = displayMember || "Name";
+                    $(element).append(new Option(item[displayHolder],item[valueHolder],false,false));
+                });
+                $(loadingIcon).css({'display':'none'});
+            },
+            error: function (e) {
+                notifyError(failErrorMessage || e.message);
+                $(loadingIcon).css({'display':'none'});
+            }
+
+        });
+    });
+}
+
